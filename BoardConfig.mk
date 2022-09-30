b8a259e7914de0852a393b61266b1e3921cb2406
ReallySnow <reallysnow233@gmail.com>
2022-09-30 14:36:57 +0800

    [DNM] sirius: permissive

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,7 @@ BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 earlycon=msm_geni_serial,0xA90000 androidboot.hardware=qcom androidboot.usbcontroller=a600000.dwc3
 BOARD_KERNEL_CMDLINE += androidboot.console=ttyMSM0 msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1
 BOARD_KERNEL_CMDLINE += service_locator.enable=1 androidboot.configfs=true loop.max_part=7 swiotlb=1
+BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_KERNEL_SEPARATED_DTBO := true
@@ -44,6 +45,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/xiaomi/sirius
 TARGET_KERNEL_CONFIG := sirius_defconfig
+
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
