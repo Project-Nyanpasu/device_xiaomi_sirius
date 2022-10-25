c0927bebc82189d9d403bb5e0bab3785ce6f6849
ReallySnow <reallysnow233@gmail.com>
2022-10-25 16:38:24 +0800

    DO NOT MERGE
    Change-Id: I7b880544cd5e799b741f932e0bf7f88a92e6f287

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_KERNEL_CONFIG := sirius_defconfig
 TARGET_KERNEL_VERSION := 4.9
 
 # TODO: Fix CAF ROM SELinux, When finish remove it
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
+#BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 # Platform
 TARGET_BOARD_PLATFORM := sdm710
